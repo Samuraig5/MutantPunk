6a78e84661367800e87a774b866045de655b3839
Samuraig5 <gian@qualit.ch>
2022-10-09 00:04:14 +0200

    Implemented a basic terminal-esque window

---
+++ src/Main/RenderLogic/Console.java
@@ -0,0 +1,204 @@
+package Main.RenderLogic;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+public class Console
+{
+    public JFrame frame;
+    public JTextPane console;
+    public JTextField input;
+    public JScrollPane scrollPane;
+
+    public StyledDocument styledDocument;
+
+    boolean trace = false;
+
+    ArrayList<String> recentUsed = new ArrayList<>();
+    int recentUsedID = 0;
+    int recentUsedMax = 10;
+
+    public Console()
+    {
+        try
+        {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); //Make the window's look match the OS
+        }
+        catch (Exception e)
+        {
+            System.out.println("Console.java ant find SystemLookAndFeel");
+        }
+
+        frame = new JFrame();
+        frame.setTitle("Mutant Punk");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Program stops when window is closed
+
+        console = new JTextPane();
+        console.setEditable(false);
+        console.setFont(new Font("Courier New", Font.PLAIN, 12));
+        console.setOpaque(false);
+
+        styledDocument = console.getStyledDocument();
+
+        input = new JTextField();
+        input.setEditable(true);
+        input.setPreferredSize(new Dimension(100,20));
+        input.setFont(new Font("Courier New", Font.PLAIN, 12));
+        input.setBorder(new LineBorder(Color.gray,1));
+        input.setForeground(Color.lightGray);
+        input.setCaretColor(Color.lightGray);
+        input.setOpaque(false);
+
+        input.addActionListener(e ->
+        {
+            String text = input.getText();
+            if(text.length() > 0)
+            {
+                recentUsed.add(text);
+                recentUsedID = 0;
+
+                doCommand(text);
+                scrollBottom();
+                input.selectAll();
+            }
+        });
+
+        input.addKeyListener(new KeyListener()
+        {
+            @Override
+            public void keyTyped(KeyEvent e)
+            {
+
+            }
+            @Override
+            public void keyPressed(KeyEvent e)
+            {
+                if(e.getKeyCode() == KeyEvent.VK_UP)
+                {
+                    if(recentUsedID < (recentUsedMax-1) && recentUsedID < (recentUsed.size()-1))
+                    {
+                        recentUsedID++;
+                    }
+                    input.setText(recentUsed.get(recentUsed.size()-1-recentUsedID));
+                }
+                else if(e.getKeyCode() == KeyEvent.VK_DOWN)
+                {
+                    if(recentUsedID > 0)
+                    {
+                        recentUsedID--;
+                    }
+                    input.setText(recentUsed.get(recentUsed.size()-1-recentUsedID));
+                }
+            }
+            @Override
+            public void keyReleased(KeyEvent e)
+            {
+
+            }
+        });
+
+        scrollPane = new JScrollPane(console);
+        scrollPane.setBorder(null);
+        scrollPane.setOpaque(false);
+        scrollPane.getViewport().setOpaque(false);
+
+        frame.add(input, BorderLayout.SOUTH);
+        frame.add(scrollPane, BorderLayout.CENTER);
+        frame.getContentPane().setBackground(new Color(50,50,50));
+
+        //Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        //frame.setSize(screenSize.width, screenSize.height);
+        frame.setSize(1000, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+    public void doCommand(String s)
+    {
+        final String[] commands = s.split(" ");
+        try
+        {
+            if(commands[0].equalsIgnoreCase("help"))
+            {
+                println("'clear' -> clears all messages of the window", false, Color.PINK);
+            }
+            else if(commands[0].equalsIgnoreCase("clear"))
+            {
+                clear();
+            }
+            else
+            {
+                println(s, false, Color.lightGray);
+            }
+        }
+        catch (Exception e)
+        {
+            println("Error ->" + e.getMessage(), trace, new Color(255,155,155));
+        }
+    }
+
+    public void scrollTop()
+    {
+        console.setCaretPosition(0);
+    }
+    public void scrollBottom()
+    {
+        console.setCaretPosition(console.getDocument().getLength());
+    }
+
+    public void print(String s, boolean trace, Color c)
+    {
+        Style style = console.addStyle("Style", null);
+        StyleConstants.setForeground(style, c);
+        if (trace)
+        {
+            Throwable t = new Throwable();
+            StackTraceElement[] elements = t.getStackTrace();
+            String caller = elements[0].getClassName();
+            s = caller + " -> " + s;
+        }
+        try
+        {
+            styledDocument.insertString(styledDocument.getLength(), s, style);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Console.java cant insert String int styledDocument");
+        }
+    }
+    public void print(String s, boolean trace)
+    {
+        print(s, trace, Color.lightGray);
+    }
+
+    public void println(String s, boolean trace, Color c)
+    {
+        print(s+"\n",trace,c);
+    }
+    public void println(String s, boolean trace)
+    {
+        println(s,trace,Color.lightGray);
+    }
+
+    public void clear()
+    {
+        try
+        {
+            styledDocument.remove(0,styledDocument.getLength());
+        }
+        catch (Exception e)
+        {
+            System.out.println("Console.java cant clear styledDocument");
+        }
+    }
+}
---
+++ src/Test/RenderLogic/ConsoleTest.java
@@ -0,0 +1,11 @@
+package Test.RenderLogic;
+
+import Main.RenderLogic.Console;
+
+public class ConsoleTest
+{
+    public static void main(String[] args)
+    {
+        new Console();
+    }
+}
