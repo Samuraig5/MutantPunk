4c3ccb752a3bd976ee19d1dd5b22fa279b9f34de
Gian Moser <104901510+Samuraig5@users.noreply.github.com>
2022-10-21 10:55:05 +0200

    Implemented the very beginnings of the world logic with Call.java which will hold the information of each cell in the game.

---
+++ src/Main/WorldLogic/Cell.java
@@ -0,0 +1,38 @@
+package Main.WorldLogic;
+
+import Main.BodyLogic.Person;
+
+import java.util.List;
+
+public class Cell
+{
+    private final int[] coordinates = new int[2];
+    private final List<Person> people;
+
+    public Cell(int[] xy, List<Person> personList)
+    {
+        coordinates[0] = xy[0];
+        coordinates[1] = xy[1];
+        people = personList;
+    }
+
+    public int[] getCoordinates()
+    {
+        return coordinates;
+    }
+    public List<Person> getPeople()
+    {
+        return people;
+    }
+    public void personEnters(Person p)
+    {
+        if(!people.contains(p))
+        {
+            people.add(p);
+        }
+    }
+    public void personLeaves(Person p)
+    {
+        people.remove(p);
+    }
+}
